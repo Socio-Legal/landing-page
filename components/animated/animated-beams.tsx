73bdf5106c63f4b4cb28f7d0582bad1065fd1445
Sttok Devs <info@sttok.com>
2024-09-24 01:29:07 +0200

    Add cloud variant

--- components/animated/animated-beams.tsx
+++ components/animated/animated-beams.tsx
@@ -1,6 +1,7 @@
 import { AnimatedBeamMultiToMulti } from "./animated-beam-multi-to-multi";
 import { AnimatedBeamMultiToSingle } from "./animated-beam-multi-to-single";
 import { AnimatedBeamSingleToMulti } from "./animated-beam-single-to-multi";
+import { AnimatedCloud } from "./animated-cloud";
 
 export const AnimatedBeams = {
   AnimatedBeam01: (
@@ -12,6 +13,7 @@ export const AnimatedBeams = {
   AnimatedBeam03: (
     <AnimatedBeamMultiToMulti className="border-0 p-0 h-[240px] mb-16" />
   ),
+  AnimatedCloud: <AnimatedCloud />,
 };
 
 export const getAnimatedBeam = (name: keyof typeof AnimatedBeams) => {
---
+++ components/animated/animated-cloud.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import { useTheme } from "next-themes";
+import {
+  Cloud,
+  fetchSimpleIcons,
+  ICloud,
+  renderSimpleIcon,
+  SimpleIcon,
+} from "react-icon-cloud";
+
+export const cloudProps: Omit<ICloud, "children"> = {
+  containerProps: {
+    style: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: 280,
+      paddingTop: 0,
+      height: 240,
+      marginBottom: 64,
+    },
+  },
+  options: {
+    reverse: true,
+    depth: 1,
+    wheelZoom: false,
+    imageScale: 2,
+    activeCursor: "default",
+    tooltip: "native",
+    initial: [0.1, -0.1],
+    clickToFront: 500,
+    tooltipDelay: 0,
+    outlineColour: "#0000",
+    maxSpeed: 0.04,
+    minSpeed: 0.02,
+    // dragControl: false,
+  },
+};
+
+export const renderCustomIcon = (icon: SimpleIcon, theme: string) => {
+  const bgHex = theme === "light" ? "#f3f2ef" : "#080510";
+  const fallbackHex = theme === "light" ? "#6e6e73" : "#ffffff";
+  const minContrastRatio = theme === "dark" ? 2 : 1.2;
+
+  return renderSimpleIcon({
+    icon,
+    bgHex,
+    fallbackHex,
+    minContrastRatio,
+    size: 42,
+    aProps: {
+      href: undefined,
+      target: undefined,
+      rel: undefined,
+      onClick: (e: any) => e.preventDefault(),
+    },
+  });
+};
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+export type DynamicCloudProps = {
+  iconSlugs?: string[];
+};
+
+type IconData = Awaited<ReturnType<typeof fetchSimpleIcons>>;
+
+export const AnimatedCloud = ({ iconSlugs }: DynamicCloudProps) => {
+  const [data, setData] = useState<IconData | null>(null);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    fetchSimpleIcons({ slugs: iconSlugs || slugs }).then(setData);
+  }, [iconSlugs]);
+
+  const renderedIcons = useMemo(() => {
+    if (!data) return null;
+
+    return Object.values(data.simpleIcons).map((icon) =>
+      renderCustomIcon(icon, theme || "light")
+    );
+  }, [data, theme]);
+
+  return (
+    // @ts-ignore
+    <Cloud {...cloudProps}>
+      <>{renderedIcons}</>
+    </Cloud>
+  );
+};
