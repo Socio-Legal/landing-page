f4f1d03c2902b7cfc70b43d56a60e7f7b1d844a6
Sttok Devs <info@sttok.com>
2024-09-24 08:46:34 +0200

    Remove shadow from animated beams

--- components/animated/animated-beams.tsx
+++ components/animated/animated-beams.tsx
@@ -5,13 +5,13 @@ import { AnimatedCloud } from "./animated-cloud";
 
 export const AnimatedBeams = {
   AnimatedBeam01: (
-    <AnimatedBeamSingleToMulti className="border-0 p-0 h-[240px] mb-16" />
+    <AnimatedBeamSingleToMulti className="border-0 p-0 h-[240px] mb-16 md:shadow-none" />
   ),
   AnimatedBeam02: (
-    <AnimatedBeamMultiToSingle className="border-0 p-0 h-[240px] mb-16" />
+    <AnimatedBeamMultiToSingle className="border-0 p-0 h-[240px] mb-16 md:shadow-none" />
   ),
   AnimatedBeam03: (
-    <AnimatedBeamMultiToMulti className="border-0 p-0 h-[240px] mb-16" />
+    <AnimatedBeamMultiToMulti className="border-0 p-0 h-[240px] mb-16 md:shadow-none" />
   ),
   AnimatedCloud: <AnimatedCloud />,
 };
