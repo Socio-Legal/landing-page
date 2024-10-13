5f62b4ecf3ad5e6310ef15b9efd1d28d23f0a5fb
Sttok Devs <info@sttok.com>
2024-10-13 07:18:23 +0200

    Update lang switcher

--- components/language-switcher.tsx
+++ components/language-switcher.tsx
@@ -79,32 +79,29 @@ const LanguageSwitcher = () => {
 
   return (
     <div
-      className="relative rounded-full border-2 border-transparent transition-colors duration-300 hover:border-primary dark:hover:border-primary"
+      className="relative rounded-full"
       ref={dropdownRef}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <button
         onClick={toggleDropdown}
-        className="relative flex h-10 w-10 items-center justify-center rounded-full border-stroke transition-colors duration-300 hover:text-primary dark:border-strokedark dark:text-white dark:hover:text-primary"
+        className="flex items-center justify-center rounded-full cursor-pointer bg-transparent dark:bg-dark-bg w-10 h-10 text-primary dark:text-primary border-2 border-transparent hover:border-brand dark:hover:border-brand transition-colors duration-300"
       >
         <Icon icon="tabler:language" width="24" height="24" />
       </button>
 
       <div
-        className={`fixed left-0 right-0 top-16 mt-2.5 flex h-auto w-full flex-col rounded-2xl border border-stroke bg-white pb-2 shadow-default transition-all duration-300 dark:border-strokedark dark:bg-dark sm:absolute sm:left-auto sm:top-full sm:w-45 sm:rounded-2xl ${
+        className={`fixed left-0 right-0 top-16 mt-2.5 flex px-6 py-4 h-auto min-w-[180px] w-full flex-col rounded-md border bg-popover text-popover-foreground shadow-lg transition-all duration-300 sm:absolute sm:left-auto sm:top-full sm:w-45 sm:rounded-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 ${
           isOpen ? "visible opacity-100" : "invisible opacity-0"
         }`}
       >
-        <div className="px-4.5 py-3">
-          <h5 className="text-sm font-medium text-bodydark2">
-            {t("language.selectLanguage")}
-          </h5>
-        </div>
-
         <ul className="flex flex-col">
           {languages.map((lang) => (
-            <li key={lang.code}>
+            <li
+              key={lang.code}
+              className="group inline-flex h-10 w-max items-center justify-center rounded-md  px-4 py-2 text-sm font-medium transition-colors hover:bg-primary/10 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-primary/10 data-[state=open]:bg-primary/10"
+            >
               <button
                 className="flex w-full cursor-pointer items-center gap-2.5 px-4.5 py-2 hover:bg-gray-2 dark:hover:bg-meta-4"
                 onClick={() => changeLanguage(lang.code)}
