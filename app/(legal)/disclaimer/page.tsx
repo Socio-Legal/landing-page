66e04e5d8f71203301f73cfa0cb581c1685d00ee
Sttok Devs <info@sttok.com>
2024-10-13 20:42:52 +0200

    Remove server side

--- app/(legal)/disclaimer/page.tsx
+++ app/(legal)/disclaimer/page.tsx
@@ -2,14 +2,12 @@
 
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import HeroSection, { HeroSectionProps } from "@/components/legal/hero-section";
 import TextContentSection, {
   TextSectionProps,
 } from "@/components/legal/text-content";
 import CallToActionSection from "@/components/landing/cta-section";
-import { GetStaticProps } from "next";
 
 const Page = () => {
   const { t } = useTranslation("legal-disclaimer-page");
@@ -26,14 +24,5 @@ const Page = () => {
     </>
   );
 };
-
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale ?? "es", [
-      "common",
-      "legal-disclaimer-page",
-    ])),
-  },
-});
 
 export default Page;
