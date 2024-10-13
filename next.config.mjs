baa7bbe4d2f34252a4d3a2e4fcf288c7614a64e0
Sttok Devs <info@sttok.com>
2024-10-13 19:34:03 +0200

    Add localePath to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
   },
+  localePath: path.resolve(__dirname, "./public/locales"),
+
   async rewrites() {
     return [
       // Products
