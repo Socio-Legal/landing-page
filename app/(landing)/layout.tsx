db435cd8ba65f063fd29e882f956608889ad8964
Sttok Devs <info@sttok.com>
2024-11-14 12:22:04 +0100

    Add metadata to home page

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -6,6 +6,15 @@ interface MarketingLayoutProps {
   children: React.ReactNode;
 }
 
+export const metadata = {
+  title: "Sttok",
+  keywords:
+    "gestión sociedades, libro de socios, captable, juntas de accionistas, corporate governance, software empresarial, administración empresas, gestión documental, cumplimiento normativo, software gestión",
+  description:
+    "Sttok - Software integral de gestión de sociedades. Simplifica la administración, cumplimiento normativo y gestión documental de tu empresa.",
+  author: "Sttok",
+};
+
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
